1ab3f74539a208f9356a3b2279f4ee1945bddefe
Gedalya Krycer <gedalya@krycer.com>
2020-08-23 00:42:01 -0700

    Added social to navbar

--- gk-portfolio/src/components/TopNavbar/index.js
+++ gk-portfolio/src/components/TopNavbar/index.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { FaGithub } from 'react-icons/fa';
+import { LinkContainer } from "react-router-bootstrap";
+import { FaGithub, FaLinkedinIn, FaBehance } from 'react-icons/fa';
 import './style.css';
 
 // This is static top section of the page and gives it semantic value.
 function TopNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
-      </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse>
+                <Nav>
+                    <Nav.Link href="https://www.linkedin.com/in/gedalyakrycer/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></Nav.Link>
+                    <Nav.Link href="https://github.com/GedalyaKrycer" target="_blank" rel="noopener noreferrer"><FaGithub /></Nav.Link>
+                    <Nav.Link href="https://www.behance.net/gedalyakrycer" target="_blank" rel="noopener noreferrer"><FaBehance /></Nav.Link>
+
+                </Nav>
+            </Navbar.Collapse>
+
+
         </Navbar>
     )
 }

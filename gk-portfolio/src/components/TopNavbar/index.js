78eaaba8bd33714bfa777e1bfdc4b384418ec4f6
Gedalya Krycer <gedalya@krycer.com>
2020-08-23 01:02:46 -0700

    Added logo to nav bar

--- gk-portfolio/src/components/TopNavbar/index.js
+++ gk-portfolio/src/components/TopNavbar/index.js
@@ -9,17 +9,22 @@ import './style.css';
 function TopNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-
+            <LinkContainer to="/" className="d-lg-none">
+                <Navbar.Brand>
+                    <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo-white.svg" alt="GK Logo" />
+                </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
-            
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
+            <LinkContainer to="/" className="d-none d-lg-block">
+                <Navbar.Brand>
+                    <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo-white.svg" alt="GK Logo" />
+                </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Collapse className="justify-end">
                 <Nav>
                     <Nav.Link href="https://www.linkedin.com/in/gedalyakrycer/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></Nav.Link>
@@ -28,6 +33,7 @@ function TopNavbar() {
 
                 </Nav>
             </Navbar.Collapse>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
 
         </Navbar>

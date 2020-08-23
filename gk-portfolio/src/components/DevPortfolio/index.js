a66814462a1371890c164c93b1bc3d8572e94624
Gedalya Krycer <gedalya@krycer.com>
2020-08-23 13:19:10 -0700

    Added project card styling

--- gk-portfolio/src/components/DevPortfolio/index.js
+++ gk-portfolio/src/components/DevPortfolio/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './style.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import DevProjectCard from '../../components/DevProjectCard';
 
 // This is static top section of the page and gives it semantic value.
@@ -12,10 +11,11 @@ function DevPortfolio() {
             <Container>
                 <h2>Dev Work</h2>
 
-                <Row className="justify-content-md-center negative-margin">
-                    <Col lg={8}>
+                <Row className="justify-content-md-around negative-margin">
+                   
                         <DevProjectCard />
-                    </Col>
+           
+               
                 </Row>
 
             </Container>
--- gk-portfolio/src/components/DevProjectCard/index.js
+++ gk-portfolio/src/components/DevProjectCard/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { FaGithub, FaDesktop } from 'react-icons/fa';
 
 
 // This is static top section of the page and gives it semantic value.
 function DevProjectCard() {
+
+    const [cardSlide, setCardSlide] = useState(false);
+
+    const cardSlider = () => !cardSlide ? setCardSlide(true) : setCardSlide(false);
+
     return (
         <div>
-            <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/react/gk-portfolio/public/assets/images/img-links/portfolio/Card/directory-thumbnail.jpg" alt="Name" className="projet-img "/>
+            <button className={`cardSlide ${cardSlide ? "cardSlideUp" : ""}`} onClick={cardSlider}>
+                <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/react/gk-portfolio/public/assets/images/img-links/portfolio/Card/directory-thumbnail.jpg" alt="Name" className="projet-img" />
+            </button>
+
+            <div className="meta-container white-color">
+                <div className="project-links">
+                    <a href="https://google.com"
+                        target="_blank"
+                        rel="noopener noreferrer" className="button">
+                        <FaDesktop />
+                        <h5>View Deployed</h5>
+                    </a>
+                    <a href="https://google.com"
+                        target="_blank"
+                        rel="noopener noreferrer" className="button">
+                            <FaGithub />
+                        <h5>View Repo</h5>
+                    </a>
+                </div>
+                <div>
+                    <h4>DigiBit Team Directory</h4>
+                    <h5>React.js</h5>
+                </div>
+            </div>
 
         </div>
     )

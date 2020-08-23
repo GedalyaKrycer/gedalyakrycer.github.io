47a9a7d4c355de870bc2b7109f16019ac28d2798
Gedalya Krycer <gedalya@krycer.com>
2020-08-23 15:47:13 -0700

    created react build

---
+++ gk-portfolio/build/precache-manifest.f931f41c33c6387292190b741ba0b72f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717344358bbc65c815d7b13702efeec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ce3fd1de52b48a75739",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e47ece10059a1d6e519b",
+    "url": "/static/css/main.dc063f48.chunk.css"
+  },
+  {
+    "revision": "7ce3fd1de52b48a75739",
+    "url": "/static/js/2.6b315e3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6b315e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47ece10059a1d6e519b",
+    "url": "/static/js/main.a94421eb.chunk.js"
+  },
+  {
+    "revision": "82c2a412192a33a84097",
+    "url": "/static/js/runtime-main.14188556.js"
+  }
+]);

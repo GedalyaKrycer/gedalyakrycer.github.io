08927b0fc02fe1a2b4d20aa17dca56682d1d04aa
Gedalya Krycer <gedalya@krycer.com>
2020-08-23 03:05:50 -0700

    Added footer

--- gk-portfolio/src/components/Footer/index.js
+++ gk-portfolio/src/components/Footer/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
-
-import { FaGithub } from 'react-icons/fa';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from "react-router-bootstrap";
+import { FaGithub, FaLinkedinIn, FaBehance } from 'react-icons/fa';
 import './style.css';
 
 // This is static top section of the page and gives it semantic value.
-function Footer() {
+function TopNavbar() {
     return (
-        <header className="bg--image">
-            
-        </header>
+        <footer className="d-flex justify-content-between">
+
+            <LinkContainer to="/">
+
+                <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo.svg" alt="GK Logo" />
+
+            </LinkContainer>
+
+
+            <a href="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/react/gk-portfolio/public/assets/pdfs/Gedalya%20Krycer%20Resume.pdf" target="_blank" rel="noopener noreferrer" className="resume"><h6>Download Resume</h6></a>
+
+
+
+            <div className="d-flex justify-content-md-between">
+                <a href="https://www.linkedin.com/in/gedalyakrycer/" target="_blank" rel="noopener noreferrer" className="social"><FaLinkedinIn /></a>
+                <a href="https://github.com/GedalyaKrycer" target="_blank" rel="noopener noreferrer" className="social"><FaGithub /></a>
+                <a href="https://www.behance.net/gedalyakrycer" target="_blank" rel="noopener noreferrer" className="social"><FaBehance /></a>
+
+            </div>
+
+        </footer>
     )
 }
 
 
-export default Footer;
+export default TopNavbar;
--- gk-portfolio/src/components/TopNavbar/index.js
+++ gk-portfolio/src/components/TopNavbar/index.js
@@ -8,9 +8,9 @@ import './style.css';
 // This is static top section of the page and gives it semantic value.
 function TopNavbar() {
     return (
-        <Navbar collapseOnSelect expand="lg">
+        <Navbar collapseOnSelect expand="md">
             
-            <LinkContainer to="/" className="d-lg-none">
+            <LinkContainer to="/" className="d-md-none">
                 <Navbar.Brand>
                     <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo-white.svg" alt="GK Logo" />
                 </Navbar.Brand>
@@ -27,13 +27,13 @@ function TopNavbar() {
                     </LinkContainer>
                 </Nav>
             </Navbar.Collapse>
-            <LinkContainer to="/" className="d-none d-lg-block">
+            <LinkContainer to="/" className="d-none d-md-block">
                 <Navbar.Brand>
                     <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo-white.svg" alt="GK Logo" />
                 </Navbar.Brand>
             </LinkContainer>
             <Navbar.Collapse className="justify-end">
-                <Nav className="social">
+                <Nav>
                     <Nav.Link href="https://www.linkedin.com/in/gedalyakrycer/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn/></Nav.Link>
                     <Nav.Link href="https://github.com/GedalyaKrycer" target="_blank" rel="noopener noreferrer" ><FaGithub /></Nav.Link>
                     <Nav.Link href="https://www.behance.net/gedalyakrycer" target="_blank" rel="noopener noreferrer"><FaBehance /></Nav.Link>

552e8d18333816f9be722f04424770ee58c4acb7
Gedalya Krycer <gedalya@krycer.com>
2020-08-23 03:24:54 -0700

    Updated footer

--- gk-portfolio/src/components/Footer/index.js
+++ gk-portfolio/src/components/Footer/index.js
@@ -10,16 +10,12 @@ function TopNavbar() {
     return (
         <footer className="d-flex justify-content-between">
 
-            <LinkContainer to="/">
-
-                <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo.svg" alt="GK Logo" />
-
-            </LinkContainer>
-
-
             <a href="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/react/gk-portfolio/public/assets/pdfs/Gedalya%20Krycer%20Resume.pdf" target="_blank" rel="noopener noreferrer" className="resume"><h6>Download Resume</h6></a>
 
 
+            <LinkContainer to="/">
+                <img src="https://raw.githubusercontent.com/GedalyaKrycer/gedalyakrycer.github.io/46e78fe53858d535b8db1d9e7e42455fa0a8b090/gk-portfolio/public/assets/images/img-links/gk-logo.svg" alt="GK Logo" />
+            </LinkContainer>
 
             <div className="d-flex justify-content-md-between">
                 <a href="https://www.linkedin.com/in/gedalyakrycer/" target="_blank" rel="noopener noreferrer" className="social"><FaLinkedinIn /></a>
---
+++ gk-portfolio/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './style.css';
+
+// This is static top section of the page and gives it semantic value.
+function Header() {
+    return (
+        
+    )
+}
+
+
+export default Header;

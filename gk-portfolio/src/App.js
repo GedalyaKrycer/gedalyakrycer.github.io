ff03102bf7cd5372cbd36e899e1fbf92390cc392
Gedalya Krycer <gedalya@krycer.com>
2020-09-22 14:09:42 -0700

    Added OG info and cleaned up old styles

--- gk-portfolio/src/App.js
+++ gk-portfolio/src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import TopNavbar from "./components/TopNavbar";
 import Footer from "./components/Footer";
+import ReactGA from 'react-ga';
 
 
 function App() {
+  // Google Analytics Page Tracking
+  useEffect(() => {
+    ReactGA.initialize('UA-122656834-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
   return (
     <Router>
 

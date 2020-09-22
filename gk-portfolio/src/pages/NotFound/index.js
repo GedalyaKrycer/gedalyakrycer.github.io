57c4f2755a5d073968a1eb56b32b30759b722269
Gedalya Krycer <gedalya@krycer.com>
2020-09-22 14:18:03 -0700

    Fixed footer not pinning

--- gk-portfolio/src/pages/NotFound/index.js
+++ gk-portfolio/src/pages/NotFound/index.js
@@ -5,13 +5,13 @@ import Header from '../../components/Header';
 // This is static top section of the page and gives it semantic value.
 function NotFound() {
     return (
-        <>
+        <div className="g__footer-pin">
             <Header
                 title="404"
                 subTitleOne={`Oops! Looks like this page does not exist or is under maintenance.`}
             />
            
-        </>
+        </div>
     )
 }
 

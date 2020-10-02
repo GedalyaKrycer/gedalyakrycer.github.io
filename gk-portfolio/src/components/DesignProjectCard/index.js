c6cf70e7f9ecd4c41e0cd45e630dff91ef74cc73
Gedalya Krycer <gedalya@krycer.com>
2020-10-02 01:32:00 -0700

    Added figma design project

--- gk-portfolio/src/components/DesignProjectCard/index.js
+++ gk-portfolio/src/components/DesignProjectCard/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import { FaArrowRight } from 'react-icons/fa';
 import videoMp4 from '../../motion-design-web.mp4';
 import videoWebp from '../../motion-design-web.webp';
+import figmaThumbnail from '../../figma-thumbnail.jpg';
 
 
 // This is static top section of the page and gives it semantic value.
@@ -11,24 +12,42 @@ function DesignPortfolio() {
 
 
     return (
+        <>
         <div className="design-project__container ">
             <a href="http://gedalya.myportfolio.com/"
                 target="_blank"
                 rel="noopener noreferrer" >
-                <div className="g_card-slide  design-project__card-slide">
+                <div className="g__design-project__card-slide">
                     <video autoPlay={true} loop={true} muted={true} playsInline={true} className="design-project__project-img">
                         <source src={videoWebp} type="video/webm" />
                         <source src={videoMp4} type="video/mp4" />
                     </video>
                 </div>
 
-                <div className="g_meta-container design-project__meta-container">
+                <div className="design-project__meta-container">
 
                     <h4 className="design-project__text">View Design Projects</h4>
                     <FaArrowRight className="design-project__icon" />
                 </div>
             </a>
         </div>
+        <div className="design-project__container ">
+            <a href="https://youtu.be/7nmarPXV2OQ"
+                target="_blank"
+                rel="noopener noreferrer" >
+                <div className="g__design-project__card-slide">
+                    <img src={figmaThumbnail} alt="Figma image" className="design-project__project-img" />
+                   
+                </div>
+
+                <div className="design-project__meta-container">
+
+                    <h4 className="design-project__text">Watch my workshop</h4>
+                    <FaArrowRight className="design-project__icon" />
+                </div>
+            </a>
+        </div>
+        </>
 
     )
 }

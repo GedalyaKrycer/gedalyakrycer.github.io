84d8bd859776604fd836013dd356c50801c6c76e
Gedalya Krycer <gedalya@krycer.com>
2020-10-10 02:47:39 -0700

    added base filter nav styles

--- gk-portfolio/src/components/TechFilter/index.js
+++ gk-portfolio/src/components/TechFilter/index.js
@@ -4,11 +4,23 @@ import './style.css';
 
 function TechFilter() {
 
-
+  const filterActive = false;
 
   return (
 
-    <div></div>
+    <div className="tech-filter__container">
+      <div className="tech-filter__nav">
+        <button className={`tech-filter__nav-dev ${filterActive ? "tech-filter__nav-dev--active" : null}`}>
+          Development
+        </button>
+        <button className={`tech-filter__nav-design ${filterActive ? "tech-filter__nav-design--active": null}`}>
+          Design
+        </button>
+        <button className={`tech-filter__nav-learn ${filterActive ? "tech-filter__nav-learn--active" : null}`}>
+          Learning
+        </button>
+      </div>
+    </div>
 
 
 

3cbfc59151c801d90896a431b0d654c98dda54c5
Gedalya Krycer <gedalya@krycer.com>
2020-10-11 17:30:57 -0700

    Added pinned github icons

--- gk-portfolio/src/components/GithubPin/index.js
+++ gk-portfolio/src/components/GithubPin/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './style.css';
+import { FaGithub, FaLinkedinIn, FaBehance } from 'react-icons/fa';
 
 
 function GithubPin() {
     return (
-     <div className="githubPin">
-         Github
-     </div>
+        <div className="githubPin">
+            <a
+                href="https://www.linkedin.com/in/gedalyakrycer/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="githubPin_link"
+            >
+                <FaLinkedinIn />
+            </a>
+            <a
+                href="https://github.com/GedalyaKrycer"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="githubPin_link"
+            >
+                <FaGithub />
+            </a>
+            <a
+                href="https://www.behance.net/gedalyakrycer"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="githubPin_link"
+            >
+                <FaBehance />
+            </a>
+
+        </div>
     )
 }
 

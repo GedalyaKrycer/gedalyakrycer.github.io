2561694ed981df2da140541e353f164e1246b10d
Gedalya Krycer <gedalya@krycer.com>
2020-10-21 16:57:27 -0700

    Added header animations

--- gk-portfolio/src/components/Header/index.js
+++ gk-portfolio/src/components/Header/index.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { gsap } from "gsap";
 import './style.css';
 
 // This is static top section of the page and gives it semantic value.
-function Header({title, subTitleOne, subTitleTwo}) {
+function Header({ title, subTitleOne, subTitleTwo }) {
+
+    // GSAP ANIMATIONS
+    const tl = gsap.timeline();
+
+    // Ref for blue line
+    const ruleRef = useRef(null);
+
+    // Ref for h1 line
+    const h1Ref = useRef(null);
+
+    // Ref for h3 line
+    const h3Ref = useRef(null);
+
+
+    useEffect(() => {
+
+        tl.from(ruleRef.current, {
+            duration: 1,
+            autoAlpha: 0,
+            scale: 4,
+            ease: 'back.out(3)'
+            
+        })
+            .to(ruleRef.current, {
+                duration: 1,
+                width: 210,
+                borderRadius: 0,
+                ease: 'back.out(3)'
+            })
+
+        tl.from(h1Ref.current, {
+            duration: 1, 
+            autoAlpha: 0,
+            y: 20,
+            ease: 'back.out(3)'
+        }, '-=1')
+
+        tl.from(h3Ref.current, {
+            duration: 1, 
+            autoAlpha: 0,
+            y: -20,
+            ease: 'back.out(3)'
+        }, '-=1')
+
+
+
+    }, [tl])
+
+
     return (
         <header>
-            <h1 className="g_white-color">{title}</h1>
-            <hr />
-            <h3 className="header__sub-title">{subTitleOne}</h3>
-            <h3 className="header__sub-title">{subTitleTwo}</h3>
+            <h1 className="g_white-color" ref={h1Ref}>{title}</h1>
+            <hr ref={ruleRef} />
+            <div ref={h3Ref}>
+                <h3 className="header__sub-title">{subTitleOne}</h3>
+                <h3 className="header__sub-title">{subTitleTwo}</h3>
+            </div>
+
         </header>
     )
 }

0349f0bb0de032f930de91ec78747c3cc2692141
Gedalya Krycer <gedalya@krycer.com>
2020-10-22 16:28:47 -0700

    Added animation to design sections

--- gk-portfolio/src/components/DesignProjectCard/index.js
+++ gk-portfolio/src/components/DesignProjectCard/index.js
@@ -12,85 +12,85 @@ gsap.registerPlugin(ScrollTrigger);
 // This is static top section of the page and gives it semantic value.
 function DesignPortfolio() {
 
-     // GSAP ANIMATIONS
+    // GSAP ANIMATIONS
 
-     // Ref for design card
-     const designCard1Ref = useRef(null);
- 
-     // Ref for design card
-     const designCard2Ref = useRef(null);
- 
-     useEffect(() => {
-         gsap.from(designCard1Ref.current, {
+    // Ref for design card
+    const designCard1Ref = useRef(null);
+
+    // Ref for design card
+    const designCard2Ref = useRef(null);
+
+    useEffect(() => {
+        gsap.from(designCard1Ref.current, {
             scrollTrigger: {
                 trigger: designCard1Ref.current,
                 toggleActions: 'play none none none',
                 start: 'top bottom',
                 end: '-=50',
-                scrub: true,
-                markers: true
+                scrub: true
             },
-             duration: 2,
-             autoAlpha: 0,
-             y: 100,
-             scale: .8,
-             ease: 'power4.out'
-           
-         });
+            duration: 2,
+            autoAlpha: 0,
+            y: 100,
+            scale: .8,
+            ease: 'power4.out'
 
-        //  gsap.from(designCard2Ref.current, {
-        //      duration: 2,
-        //      autoAlpha: 0,
-        //      y: 100,
-        //      scale: .8,
-        //      ease: 'power4.out',
-        //      scrollTrigger: {
-        //          trigger: designCard2Ref.current,
-        //          start: 'top bottom-=150',
-        //          toggleActions: 'play none none reverse',
-        //          markers: true
-        //      }
-        //  });
-     }, [])
+        });
+
+        gsap.from(designCard2Ref.current, {
+            scrollTrigger: {
+                trigger: designCard2Ref.current,
+                toggleActions: 'play none none none',
+                start: 'top bottom',
+                end: '-=50',
+                scrub: true
+            },
+            duration: 2,
+            autoAlpha: 0,
+            y: 100,
+            scale: .8,
+            ease: 'power4.out'
+        });
+    }, [])
 
 
 
     return (
         <>
-        <div className="design-project__container" ref={designCard1Ref}>
-            <a href="http://gedalya.myportfolio.com/"
-                target="_blank"
-                rel="noopener noreferrer" >
-                <div className="g__design-project__card-slide">
-                    <video autoPlay={true} loop={true} muted={true} playsInline={true} className="design-project__project-img">
-                        <source src={videoWebp} type="video/webm" />
-                        <source src={videoMp4} type="video/mp4" />
-                    </video>
-                </div>
+            <div className="design-project__container" ref={designCard1Ref}>
+                <a href="http://gedalya.myportfolio.com/"
+                    target="_blank"
+                    rel="noopener noreferrer" >
+                    <div className="g__design-project__card-slide">
+                        <video autoPlay={true} loop={true} muted={true} playsInline={true} className="design-project__project-img">
+                            <source src={videoWebp} type="video/webm" />
+                            <source src={videoMp4} type="video/mp4" />
+                        </video>
+                    </div>
 
-                <div className="design-project__meta-container">
+                    <div className="design-project__meta-container">
 
-                    <h4 className="design-project__text">View design projects</h4>
-                    <FaArrowRight className="design-project__icon" />
-                </div>
-            </a>
-        </div>
-        <div className="design-project__container" ref={designCard2Ref}>
-            <a href="https://youtu.be/7nmarPXV2OQ"
-                target="_blank"
-                rel="noopener noreferrer" >
-                <div className="g__design-project__card-slide">
-                    <img src={figmaThumbnail} alt="From a Figment to Figma." className="design-project__project-img" />
-                   
-                </div>
+                        <h4 className="design-project__text">View design projects</h4>
+                        <FaArrowRight className="design-project__icon" />
+                    </div>
+                </a>
+            </div>
+            <div className="design-project__container" ref={designCard2Ref}>
+                <a href="https://youtu.be/7nmarPXV2OQ"
+                    target="_blank"
+                    rel="noopener noreferrer" >
+                    <div className="g__design-project__card-slide">
+                        <img src={figmaThumbnail} alt="From a Figment to Figma." className="design-project__project-img" />
 
-                <div className="design-project__meta-container">
+                    </div>
 
-                    <h4 className="design-project__text">Watch my workshop</h4>
-                    <FaArrowRight className="design-project__icon" />
-                </div>
-            </a>
-        </div>
+                    <div className="design-project__meta-container">
+
+                        <h4 className="design-project__text">Watch my workshop</h4>
+                        <FaArrowRight className="design-project__icon" />
+                    </div>
+                </a>
+            </div>
         </>
 
     )
